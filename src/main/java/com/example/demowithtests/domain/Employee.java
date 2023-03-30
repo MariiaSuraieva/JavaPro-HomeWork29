06bf6203cf7b4f63f5b19707da4c6eba0415a46c
MariiaSuraieva <109076514+MariiaSuraieva@users.noreply.github.com>
2023-03-30 17:40:31 +0200

    passports

---
+++ src/main/java/com/example/demowithtests/domain/Employee.java
@@ -0,0 +1,49 @@
+package com.example.demowithtests.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String country;
+    private String email;
+    private Boolean isDeleted = Boolean.FALSE;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "employee_id")
+    private Set<Address> addresses = new HashSet<>();
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "employee_id")
+    private Set<Photo> photos = new HashSet<>();
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "passport_id", referencedColumnName = "id")
+    private Passport passport;
+
+    public Employee(String name, String country, String email, Boolean isDeleted) {
+        this.name = name;
+        this.country = country;
+        this.email = email;
+        this.isDeleted = isDeleted;
+    }
+
+    public Employee(String name, String country, String email) {
+        this.name = name;
+        this.country = country;
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/example/demowithtests/service/employee/EmployeeService.java
@@ -0,0 +1,43 @@
+package com.example.demowithtests.service.employee;
+
+import com.example.demowithtests.domain.Employee;
+import com.example.demowithtests.util.UserIsNotExistException;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    Employee create(Employee employee);
+
+    List<Employee> getAll();
+
+    Employee getById(String id);
+
+    Employee updateById(Integer id, Employee plane) throws UserIsNotExistException;
+
+    void removeById(Integer id);
+
+    void removeAll();
+
+    List<Employee> sendEmailByCountry(String country, String text);
+
+    List<Employee> sendEmailByCity(String city, String text);
+
+    Employee createrEmployee(String name, String country, String email);
+
+    void fillingDataBase(String quantity);
+
+    void updaterByCountryFully(String countries);
+
+    void updaterByCountrySmart(String countries);
+
+    String randomCountry(String countriesString);
+
+    List<Employee> processor();
+
+    void emailSenderPhotoChange();
+
+    void emailSenderWhoMovedFromUkraine(String country);
+
+    Employee addPassport(Integer employeeId, Integer passportId);
+}
